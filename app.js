31fb0df4774b0a222a6bb4ff8f1089d3d16ca2d3
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-07 09:11:38 +0700

    Sprint1-init express project & nodemon setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// view engine EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware custom: mencatat setiap request yang masuk ke terminal
+app.use((req, res, next) => {
+  const waktu = new Date().toLocaleTimeString('id-ID');
+  console.log(`[${waktu}] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
+// static file (css, js, gambar)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Server Toko Sembako Ariesta berjalan');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});

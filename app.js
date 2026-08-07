4edc221dc29f48023fa849fb5625718241fb9bbc
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-07 09:16:39 +0700

    Sprint1-add semantic HTML for beranda page with EJS partials

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Server Toko Sembako Ariesta berjalan');
+  res.render('beranda');
 });
 
 app.listen(PORT, () => {
